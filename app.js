454f09aa13354ccd9bb68e667beb49760d9668c9
kreationsofimpermanence <kreations.of.impermanence@gmail.com>
2025-12-14 13:56:45 -0500

    Refactor searchChange and add renderSongs function
    Refactor searchChange function to accept an event parameter and log its value. Add renderSongs function to fetch and log song data from an API.

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
-
-console.log('wtf')
-
-function searchChange(){
-    console.log('working');
+function searchChange(event){
+    console.log(event.target.value);
 }
+
+async function renderSongs(){
+    const response = await fetch("https://phish.in/api/v2/swagger_doc")
+    const data = await response.json()
+    console.log(data.tags[5])
+}
+
+renderSongs();
