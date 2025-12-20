d09368a171ebf54416ea493a8fff18a8eb7da2bd
kreationsofimpermanence <kreations.of.impermanence@gmail.com>
2025-12-20 13:46:09 -0500

    Refactor song rendering with search functionality
    Updated renderSongs function to accept a search term and fetch data accordingly. Modified searchChange function to call renderSongs with the input value.

--- app.js
+++ app.js
@@ -1,15 +1,26 @@
+const songWrapper = document.querySelector('.songs')
+//console.log(songWrapper)
 
-const songsWrapper = document.querySelector('.songs')
-console.log(songsWrapper)
-
-function searchChange(event){
-    console.log(event.target.value);
+function searchChange(event){   //gets the item to be searched
+   renderSongs(event.target.value) //sends item typed in to the renderSongs function
 }
 
-async function renderSongs(){
-    const response = await fetch("https://phish.in/api/v2/swagger_doc")
-    const data = await response.json()
-    const songArr = data.search
-}
-
-renderSongs();
+ async function renderSongs(searchTerm){ //uses search parameters to get the song
+      const response = await fetch(`https://phish.in/api/v2/shows/${searchTerm}`) //fetches the database with the search term
+      const data = await response.json() //turns database into useable code
+      console.log(data)
+      let songArr = data.tour_name //targets the name of the tour from the database
+      
+      songWrapper.innerHTML = songArr.map((song) =>{ //maps through the array creating HTML for the site
+        return `
+        <div class="song">              
+        <img src=${song.album_cover_url} alt="">
+        <h1>${song.tour_name}</h1>
+        <h3>${song.date}</h3>
+        <h3>${song.venue_name}</h3>
+        <h3>Next Show will be on: ${song.next_show_date} Tracks</h3>
+        <button>Play</button>
+        </div>
+        `   })
+      }
+    
