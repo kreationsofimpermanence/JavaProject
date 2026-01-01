6e9a596c4c3f152b75ca7fd38795f9276c145f56
kreationsofimpermanence <kreations.of.impermanence@gmail.com>
2026-01-01 15:31:40 -0500

    Enhance song search and display logic
    Refactor song search functionality and improve display logic for exact and other shows.

--- app.js
+++ app.js
@@ -1,28 +1,64 @@
 
+const search = document.querySelector('#search')
 const songWrapper = document.querySelector('.songs')
+const songWrapper2 = document.querySelector('.other_shows')
 
 function searchChange(event){   //gets the item to be searched
    renderSongs(event.target.value) //sends item typed in to the renderSongs function
+   search.style.display = 'block' 
 }
 
- async function renderSongs(searchTerm){ //uses search parameters to get the song
-      const response = await fetch(`https://phish.in/api/v2/shows/${encodeURIComponent(searchTerm)}`) //fetches the database with the search term
-      const data = await response.json() //turns database into useable code
+async function renderSongs(searchTerm){ //uses search parameters to get the song
+    const response = await fetch(`https://phish.in/api/v2/search/${encodeURIComponent(searchTerm)}`) //fetches the database with the search term
+    const data = await response.json() //turns database into useable code
+    console.log(data)
+    let song = data //targets the name of the tour from the database
       
-      let song = data //targets the name of the tour from the database
-      
-      if(song.tour_name === undefined) {
-        songWrapper.innerHTML = "<h2>Phish did not play that day.<br>Please try another date.</h2>"
-        return
+    if(song.exact_show === null) {
+        if(song.other_shows.length === 0){
+          songWrapper.innerHTML = `<h2>Phish never played on that day. Try another date</h2>`
+          return
+        }else{
+          songWrapper.innerHTML = `<h2> They played that date before, just not that year. <br>Try: ${song.other_shows[0].date}</h2>`
+          return
+        }
       } 
-
-      songWrapper.innerHTML = 
-       `
+        
+    songWrapper.innerHTML = //displays the album cover and download link for the exact show searched
+      `
         <div class="song">              
-          <img src=${song.album_cover_url} class="album-cover" alt="${song.tour_name} Album Cover"/>
-          <h1 class="tour_name">"${song.tour_name}"</h1>
-          <h3>Next Show will be on: ${song.next_show_date}</h3>
-          ${song.album_zip_url ? `<a href="${song.album_zip_url}" class="download__btn">Download Album Here</a>` : ''}
-        </div>
-        `
+        <img src=${song.exact_show.album_cover_url} class="album-cover" alt="${song.exact_show.tour_name} Album Cover"/>
+          ${song.exact_show.album_zip_url ? `<a href="${song.exact_show.album_zip_url}" class="download__btn">Download Album Here</a>` : ''}
+      `
+    if(song.other_shows.length > 0) {//loops through other shows on that date and displays albumn covers
+      songWrapper2.innerHTML += '<br><h2 class="other_title">Other Shows on this Date:</h2><br>' 
+      for(let i = 0; i < song.other_shows.length; i++){
+        songWrapper2.innerHTML +=`
+          <div class="other_shows">
+          <img src=${song.other_shows[i].album_cover_url} class="album-cover" alt="${song.other_shows[i].tour_name} Album Cover"/>
+            ${song.other_shows[i].album_zip_url ? `<a href="${song.other_shows[i].album_zip_url}" class="download__btn">Download Album Here</a>` : ''}
+          </div>
+          `
+      }
+    }
+    
+     function filterShows(filter) {
+      if (filter === 'LOW_TO_High') {
+         console.log('Filtering shows with filter:', filter);
+        // song.other_shows.sort((a, b) => a.year - b.year);
+       } else if (filter === 'HIGH_TO_LOW') {
+         console.log('Filtering shows with filter:', filter);
+         //song.other_shows.sort((a, b) => b.year - a.year);
+       } else if (filter === 'A_TO_Z') {
+         console.log('Filtering shows with filter:', filter);
+         //song.other_shows.sort((a, b) => a.city.localeCompare(b.city));
+       }
+    }
+    
+    setTimeout( () => {
+      filterShows();
+    });
+        
+     
+       
  }
